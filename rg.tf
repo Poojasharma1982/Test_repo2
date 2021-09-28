d9f883925bccdfd3846d229342a88bd0dbebcda6
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-09-27 22:20:40 -0400

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -1,4 +1,10 @@
 resource "azurerm_resource_group" "Pooja-rg2" {
   name     = "example-2"
   location = "eastus2"
+  tags = {
+      Environment = "Test"
+      Service     = "Example"
+      month = "September"
+      day = "Monday"
+    }
 }
